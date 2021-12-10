e4b46aa2b6e78258a1d04b876a23e17816fb0905
Haleema Mughal <haleemamughal15@gmail.com>
2021-12-10 13:10:24 +0500

    M-1

--- src/pages/doctors-specialists/index.jsx
+++ src/pages/doctors-specialists/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Container from 'components/container';
+import Button from 'components/button';
 
 const DoctorsSpecialist = () => {
   return (
     <Container>
       <p>hello</p>
+      <Button label="ohoo" />
     </Container>
   );
 };
